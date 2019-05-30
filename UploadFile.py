b02ec37cb4862be2c9916b51a9257dadac0aba40
Serq0 <sergiuszjanczura@gmail.com>
2019-05-30 23:16:51 +0200

    Update UploadFile.py

--- UploadFile.py
+++ UploadFile.py
@@ -1,16 +1,11 @@
 import ftplib
 import Settings
 
-FTPaddress = Settings.FTPaddress
-username = Settings.username
-password = Settings.password
-filename = Settings.filename
-
 def Upload():
-    session = ftplib.FTP(FTPaddress,username,password)
-    file = open(filename,'rb')                  # file to send
-    session.storbinary('STOR '+filename, file)     # send the file
+    session = ftplib.FTP(Settings.FTPaddress,Settings.username,Settings.password)
+    file = open(Settings.filename,'rb')                  # file to send
+    session.storbinary('STOR '+Settings.filename, file)     # send the file
     file.close()                                    # close file and FTP
     session.quit()
-    print('File '+filename+' uploaded')
+    print('File '+Settings.filename+' uploaded')
 
